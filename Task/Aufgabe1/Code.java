244e07cf2de63c3bb3ecbf20fda021eb615f5d3a
Chris <chris.eder96@googlemail.com>
2016-07-06 13:06:15 +0200

    Mein Test ~(*-*)~

---
+++ Task/Aufgabe1/Code.java
@@ -0,0 +1,21 @@
+
+public class Code{
+	public static void main(String[] args){
+
+		int a = Integer.valueOf(args[0]);
+		System.out.println(a);
+	
+}
+
+public static String greet()
+{
+		return "Hello World!";
+}
+public static int add()
+{
+	int a = 5;
+	int b = 10;
+	return a + b;
+}
+}
+
---
+++ Task/Aufgabe1/Try.java
@@ -0,0 +1,20 @@
+
+
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class Try{
+	
+	@Test
+	public void stringTest(){
+		assertEquals("Hello World!", Code.greet());
+		
+	}
+	
+	@Test
+	public void addTest(){
+
+		assertEquals(9, Code.add());
+	}
+}
